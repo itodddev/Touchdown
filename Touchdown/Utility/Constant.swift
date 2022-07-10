265bcf01b84f49b4c2d6cecfe58231890beca713
Todd James <todd@itodd.co>
2022-07-10 15:18:27 -0400

    Initial Commit

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,29 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Todd James on 7/10/22.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+// UX
+
+// API
+
+// IMAGE
+
+// FONT
+
+// STRING
+
+// MISC
